ba189de11d41af2094061455276438de9af7d5c4
lucascapl <71772605+lucascapl@users.noreply.github.com>
2021-10-27 11:47:08 -0300

    Create duvida_recursao.py

---
+++ duvida_recursao.py
@@ -0,0 +1,25 @@
+new_list = []
+
+
+def my_filter(func,lista):
+    global new_list
+    if not lista:
+        return lista
+    else:
+        if func(lista[0]):
+            new_list.append(lista[0])
+            my_filter(func, lista[1:])
+        else:
+            my_filter(func, lista[1:])
+    return new_list
+
+def is_even(item):
+    if item>6 :
+        return True
+    else:
+        return False
+ 
+    
+
+seq=[1,2,3,4,5,6,7,8,9,10]
+print(my_filter(is_even,seq))
